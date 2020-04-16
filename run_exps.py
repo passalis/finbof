686f41b5e975ba952907e6b9470739b749a53bbb
Nikolaos Passalis <passalis@users.noreply.github.com>
2020-04-16 21:34:16 +0300

    Update run_exps.py

--- run_exps.py
+++ run_exps.py
@@ -15,8 +15,7 @@ def run_experiments(model, output_path, train_epochs=20, window=10):
     print("----------")
     print("Elapsed time = ", b - a)
     metrics_1 = get_average_metrics(results1)
-    # metrics_2 = get_average_metrics(results2)
-    # metrics_3 = get_average_metrics(results3)
+
     print("----------")
 
     with open(join("results", output_path), 'wb') as f:
@@ -26,14 +25,14 @@ def run_experiments(model, output_path, train_epochs=20, window=10):
 
 # Experiments!
 
-# model = lambda: ConvolutionalTemporalCorrelationBoFAdaptivePyramid(window=15, split_horizon=5, use_scaling=True)
-# run_experiments(model, 'final_bof.pickle', window=15)
+model = lambda: ConvolutionalTemporalCorrelationBoFAdaptivePyramid(window=15, split_horizon=5, use_scaling=True)
+run_experiments(model, 'final_bof.pickle', window=15)
 
-# model = lambda: GRU_NN()
-# run_experiments(model, 'final_gru.pickle', window=15)
-#
-# model = lambda: LSTM_NN()
-# run_experiments(model, 'final_lstm.pickle', window=15)
+model = lambda: GRU_NN()
+run_experiments(model, 'final_gru.pickle', window=15)
+
+model = lambda: LSTM_NN()
+run_experiments(model, 'final_lstm.pickle', window=15)
 
 model = lambda: CNN_NN()
 run_experiments(model, 'final_cnn.pickle', window=15)
